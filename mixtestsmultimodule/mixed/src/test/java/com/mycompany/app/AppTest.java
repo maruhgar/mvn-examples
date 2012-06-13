317832a39bcb2de25b4b95c9781d9d19f3177af6
maruhgar <maruhgar@email.com>
2012-06-13 12:28:02 +0530

    Example project for SO question http://stackoverflow.com/questions/11007906/junit4-and-testng-in-one-project-with-maven-with-inheritance

---
+++ mixtestsmultimodule/mixed/src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}
---
+++ mixtestsmultimodule/mixed/src/test/java/com/mycompany/app/AppTestNGTest.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTestNGTest
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
